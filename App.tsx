61e80cba54806b8a9822f13e580a8d0a0355d8d2
Melissad02 <dic20006@byui.edu>
2025-09-24 18:02:34 -0600

    Whole lotta work

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+const dialogueDay1 = ["Ow.","...","Nope.","You didn't hear anything.","...","...","Hey, wait's your name?","{name}, huh. Nice to meet you I'm a square.","What you think I have a real name? Nah, just square.","Listen if you come back tomorrow, I'll show you something cool."]
 
 export default function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePress = () => {
+    if (currentIndex < dialogueDay1.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Dialogue Text */}
+      <Text style={styles.dialogue}>{dialogueDay1[currentIndex]}</Text>
+      
+      {/* Pressable square */}
+      <Pressable onPress={handlePress}>
+        <View style={styles.square} />
+      </Pressable>
     </View>
   );
 }
@@ -17,4 +35,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dialogue: {
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  square: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'orange',
+    marginTop: 20,
+  },
 });
+
+function MySquare() {
+  return (
+    <Pressable>
+      <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
+      <View style={{
+      width: 100,
+      height: 100,
+      backgroundColor: 'orange',
+      alignItems: 'center'
+      }} />
+      </View>
+    </Pressable>
+  )
+}
+
+
+
